404c69f2bb3a84229db1bcf39e2c828ef3c7a287
Bruno Marques <brunolmarques@gmail.com>
2018-09-14 18:04:57 -0300

    CNN revision, top layers not connecting.

--- skin_cancer_train.py
+++ skin_cancer_train.py
@@ -1,6 +1,7 @@
 ### Algorithm that recognizes skin cancer by analysing photos
 
 # Import statements
+from keras.models import Model, Sequential
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras.layers import Dense, Activation, Dropout, Flatten
@@ -10,13 +11,11 @@ from keras.callbacks import ModelCheckpoint
 import numpy as np
 
 img_width, img_height = 256, 256
-train_data_dir = "/data/train"
-validation_data_dir = "/data/valid"
-test_data_dir = "/data/tedts
-nb_train_samples = 4125
-nb_validation_samples = 466
-batch_size = 32
-epochs = 200
+train_data_dir = "data/train"
+validation_data_dir = "data/valid"
+test_data_dir = "data/test"
+batch_size = 50
+epochs = 10
 seed = 7
 
 # Define augumentation and normalization instances
@@ -49,12 +48,10 @@ valid_generator = train_valid_datagen.flow_from_directory(
 
 test_generator = test_datagen.flow_from_directory(
                                     test_data_dir,
-                                    target_size=(img_width, img_height,
+                                    target_size=(img_width, img_height),
                                     batch_size=batch_size,
                                     seed = seed,
-                                    class_mode='None')
-
-    return train_generator, valid_generator, test_generator
+                                    class_mode='categorical')
 
 
 # Model architecture
@@ -66,31 +63,63 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # Adding model classifier
-x = base_model.output
-x = GlobalAveragePooling2D(input_shape=(7, 7, 512))(x)
-x = Dropout(rate=0.5, seed=seed)(x)
-x = Dense(1024,activation='relu')(x)
-predictions = Dense(2, activation='softmax')
-
-# Final model
-final_model = Model(input=base_model.input, output=predictions)
+top_model = Sequential()
+top_model.add(GlobalAveragePooling2D(data_format='channels_last'))
+top_model.add(Dropout(rate=0.5, seed=seed))
+top_model.add(Dense(1024))
+top_model.add(Activation('relu'))
+top_model.add(Dropout(rate=0.5, seed=seed))
+top_model.add(Dense(3))
+top_model.add(Activation('softmax'))
 
 # Compile Model
-final_model.compile(loss="categorical_crossentropy", optimizer='rmsprop',
+top_model.compile(loss="categorical_crossentropy", optimizer='RMSprop',
                     metrics=["accuracy"])
 
 # Create a checkpoint to save best Model
 checkpointer = ModelCheckpoint(filepath='dermo_vgg16.weights.best.hdf5',
                             verbose=1, save_best_only=True)
 
+# Final model
+final_model = Model(input=base_model.input, output=top_model)
+
 # Train the Model
 final_model.fit_generator(train_generator,
                             epochs=epochs,
                             validation_data=valid_generator,
                             verbose=1,
-                            callbacks=[checkpointer],
-                            shuffle=True)
+                            callbacks=[checkpointer])
 
 
 # Load the weights that yielded the best validation accuracy
 final_model.load_weights('dermo_vgg16.weights.best.hdf5')
+
+
+
+# Fine tune Model by unfreezing base model and freezing the added top layers
+'''
+for layer in final_model.layers[-4:]:
+    layer.trainable = False
+for layer in final_model.layers[:-4]:
+    layer.trainable = True
+
+# Compile Model
+final_model.compile(loss="categorical_crossentropy",
+                    optimizer=SGD(lr=0.0001, momentum=0.9),
+                    metrics=["accuracy"])
+
+# Create a checkpoint to save best Model
+checkpointers = ModelCheckpoint(filepath='dermo2_vgg16.weights.best.hdf5',
+                            verbose=1, save_best_only=True)
+
+# Train the fine tunning
+final_model.fit_generator(train_generator,
+                            epochs=epochs,
+                            validation_data=valid_generator,
+                            verbose=1,
+                            callbacks=[checkpointer2])
+'''
+
+# Save final model load_weights
+
+final_model.save_weights("trained_dermo.h5")
