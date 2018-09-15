d7a052886ef430f2a4f3fcc81c7625fdfe660d59
Bruno Marques <brunolmarques@gmail.com>
2018-09-15 16:45:47 -0300

    'Alternative model algorithm' 'Used the base model, witout the top layers to predict the images' 'labels. All weights of base model were frozen and only the new top' 'layer were trained. This was necessary because my personal computer' 'do not have enough fire power to train a full deep neural network'

--- skin_cancer_train.py
+++ skin_cancer_train.py
@@ -14,7 +14,7 @@ img_width, img_height = 256, 256
 train_data_dir = "data/train"
 validation_data_dir = "data/valid"
 test_data_dir = "data/test"
-batch_size = 50
+batch_size = 10
 epochs = 10
 seed = 7
 
@@ -62,41 +62,78 @@ base_model = VGG16(weights='imagenet', include_top=False,
 for layer in base_model.layers:
     layer.trainable = False
 
-# Adding model classifier
+# Create numpy zeros arrays to hold the training and valid features
+train_features = np.zeros(shape=(2000, 7, 7, 512))
+train_labels = np.zeros(shape=(2000,3))
+valid_features = np.zeroes(shape=(150, 7, 7, 512))
+valid_labels = np.zerps(shape=(150, 3))
+
+# Use base model to predict the output of the imageset, output will be a
+# tensor of dimension ( , 7, 7, 512)
+i=0
+for train_inputs, train_labels_batch in train_generator:
+    train_features_batch = base_model.predict(train_inputs)
+    train_features[i * batch_size : (i+1) * batch_size] = train_features_batch
+    train_labels[i * batch_size : (i+1) * batch_size] = train_labels_batch
+    i += 1
+    if i * batch_size >= 2000:
+        break
+train_features = np.reshape(train_features, (2000, 7, 7, 512))
+
+j=0
+for valid_inputs, valid_labels_batch in valid_generator:
+    valid_features_batch = base_model.predict(valid_inputs)
+    valid_features[i * batch_size : (i+1) * batch_size] = valid_features_batch
+    valid_labels[i * batch_size : (i+1) * batch_size] = valid_labels_batch
+    i += 1
+    if i * batch_size >= 2000:
+        break
+valid_features = np.reshape(valid_features, (2000, 7, 7, 512))
+
+# Adding top model classifier
 top_model = Sequential()
-top_model.add(GlobalAveragePooling2D(data_format='channels_last'))
+top_model.add(GlobalAveragePooling2D(input_shape=(7, 7, 512)))
 top_model.add(Dropout(rate=0.5, seed=seed))
-top_model.add(Dense(1024))
+top_model.add(Dense(1024)))
 top_model.add(Activation('relu'))
 top_model.add(Dropout(rate=0.5, seed=seed))
 top_model.add(Dense(3))
 top_model.add(Activation('softmax'))
 
-# Compile Model
+# Compile top model
 top_model.compile(loss="categorical_crossentropy", optimizer='RMSprop',
+                    metrics=["accuracy"])
+
+# Fit the top model with the generated data
+history = top_model.fit(train_features,
+                        train_labels,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_data=(valid_features, valid_labels))
+
+# Final model
+final_model = Model(input=base_model.input, output=predictions)
+
+# Compile Model
+final_model.compile(loss="categorical_crossentropy", optimizer='RMSprop',
                     metrics=["accuracy"])
 
 # Create a checkpoint to save best Model
 checkpointer = ModelCheckpoint(filepath='dermo_vgg16.weights.best.hdf5',
                             verbose=1, save_best_only=True)
 
-# Final model
-final_model = Model(input=base_model.input, output=top_model)
-
 # Train the Model
 final_model.fit_generator(train_generator,
-                            epochs=epochs,
-                            validation_data=valid_generator,
-                            verbose=1,
-                            callbacks=[checkpointer])
-
+            epochs=epochs,
+            validation_data=valid_generator,
+            verbose=1,
+            callbacks=[checkpointer])
 
 # Load the weights that yielded the best validation accuracy
 final_model.load_weights('dermo_vgg16.weights.best.hdf5')
 
-
-
 # Fine tune Model by unfreezing base model and freezing the added top layers
+# Requires GPU (CUDA) to process
 '''
 for layer in final_model.layers[-4:]:
     layer.trainable = False
@@ -121,5 +158,4 @@ final_model.fit_generator(train_generator,
 '''
 
 # Save final model load_weights
-
 final_model.save_weights("trained_dermo.h5")
